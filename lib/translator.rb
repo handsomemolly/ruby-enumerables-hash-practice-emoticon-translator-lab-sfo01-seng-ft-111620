2a7d70a5e6ef2baa51e6dd8a9304051b66a28300
Ryan Kendig <ryan.kendig@gmail.com>
2020-11-06 01:27:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,21 +16,9 @@ end
 
 
 def get_japanese_emoticon(path, emoticon)
-  emoticons = load_library(path) #call load_library
-  result = emoticons["get_emoticon"][emoticon]
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
+ 
 end
 
 def get_english_meaning(path, emoticon)
-  emoticons = load_library(path)
-  result = emoticons["get_meaning"][emoticon] #the same above but change get_meaning
-  if result
-    result
-  else
-    "Sorry, that emoticon was not found"
-  end
+  
 end
